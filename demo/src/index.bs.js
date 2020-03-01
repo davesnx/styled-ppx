4f1a0e857ce6695d63d116aa1720109c68285805
David Sancho <dsnxmoreno@gmail.com>
2020-03-01 17:24:36 +0100

    refactor: Simplify demo

--- demo/src/index.bs.js
+++ demo/src/index.bs.js
@@ -25,36 +25,9 @@ var Style = {
   make: Index$Style
 };
 
-var styled = Emotion.css(undefined, /* :: */[
-      Emotion.display(/* flex */-1010954439),
-      /* [] */0
-    ]);
-
-function Index$Component2(Props) {
-  var children = Props.children;
-  return React.createElement("div", {
-              className: styled
-            }, children);
-}
-
-var Component2 = {
-  styled: styled,
-  make: Index$Component2
-};
-
-function Index$Demo(Props) {
-  return React.createElement(Index$Style, {
-              children: "React API"
-            });
-}
-
-var Demo = {
-  make: Index$Demo
-};
-
-ReactDOMRe.renderToElementWithId(React.createElement(Index$Demo, { }), "app");
+ReactDOMRe.renderToElementWithId(React.createElement(Index$Style, {
+          children: "React API"
+        }), "app");
 
 exports.Style = Style;
-exports.Component2 = Component2;
-exports.Demo = Demo;
 /* styles Not a pure module */
--- demo/src/index.re
+++ demo/src/index.re
@@ -1,18 +1,8 @@
 module Style = [%styled {|display: flex; color: #333;|}];
 
-module Component2 = {
-  let styled = Emotion.(css([display(`flex)]));
-  [@react.component]
-  let make = (~children) =>
-    [@JSX] div(~className=styled, ~children=[children], ());
-};
-
-module Demo = {
-  [@react.component]
-  let make = () =>
-    <Style>
-      {React.string("React API")}
-    </Style>
-};
-
-ReactDOMRe.renderToElementWithId(<Demo />, "app");
+ReactDOMRe.renderToElementWithId(
+  <Style>
+    {React.string("React API")}
+  </Style>,
+  "app"
+);
