1edd9363f7fe7c324af26696605095984643a3e9
David Sancho <dsnxmoreno@gmail.com>
2020-05-26 11:07:26 +0200

    fix: Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function bump_all() {
-  versionNumber=$(npm version "$1")
-}
-
 function help() {
   echo "Usage: $(basename "$0") [<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease]"
 }
@@ -13,23 +9,17 @@ if [ -z "$1" ] || [ "$1" = "help" ]; then
   exit
 fi
 
-if [ -d ".git" ]; then
-  changes=$(git status --porcelain)
-  branch=$(git rev-parse --abbrev-ref HEAD)
+changes=$(git status --porcelain)
+branch=$(git rev-parse --abbrev-ref HEAD)
 
-  if [ -n "${changes}" ]; then
-    echo "Please commit staged files prior to bumping"
-    exit 1
-  elif [ "${branch}" != "master" ]; then
-    echo "Please run the release script on master"
-    exit 1
-  else
-    bump_all "$1"
-    git add .
-    git commit -m "Bump to ${versionNumber}"
-    git tag -a "${versionNumber}" -m "${versionNumber}"
-    git push origin --tags
-  fi
+if [ -n "${changes}" ]; then
+  echo "Please commit staged files prior to bumping"
+  exit 1
+elif [ "${branch}" != "master" ]; then
+  echo "Please run the release script on master"
+  exit 1
 else
-  bump_all
+  versionNumber=$(npm version "$1")
+  git tag -a "${versionNumber}" -m "${versionNumber}"
+  git push origin --tags
 fi
