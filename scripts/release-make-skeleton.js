bb6c3686ab1decab43fdebe9327527d7efec569c
David Sancho <dsnxmoreno@gmail.com>
2020-10-09 14:31:06 +0200

    chore: Change package name

--- scripts/release-make-skeleton.js
+++ scripts/release-make-skeleton.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const { execSync } = require("child_process");
-const fs = require("fs");
-const path = require("path");
-const esy = require("../package.json");
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const esy = require('../package.json');
 
-const filesToCopy = ["LICENSE", "README.md"];
+const filesToCopy = ['LICENSE', 'README.md'];
 
 function exec(cmd) {
   console.log(`exec: ${cmd}`);
@@ -24,8 +24,8 @@ function removeSync(p) {
   exec(`rm -rf "${p}"`);
 }
 
-const src = path.resolve(path.join(__dirname, ".."));
-const dst = path.resolve(path.join(__dirname, "..", "_release"));
+const src = path.resolve(path.join(__dirname, '..'));
+const dst = path.resolve(path.join(__dirname, '..', '_release'));
 
 removeSync(dst);
 mkdirpSync(dst);
@@ -37,41 +37,41 @@ for (const file of filesToCopy) {
 }
 
 fs.copyFileSync(
-  path.join(src, "scripts", "release-postinstall.js"),
-  path.join(dst, "postinstall.js")
+  path.join(src, 'scripts', 'release-postinstall.js'),
+  path.join(dst, 'postinstall.js')
 );
 
-const filesToTouch = ["styled-ppx.exe"];
+const filesToTouch = ['styled-ppx.exe'];
 
 for (const file of filesToTouch) {
   const p = path.join(dst, file);
   mkdirpSync(path.dirname(p));
-  fs.writeFileSync(p, "");
+  fs.writeFileSync(p, '');
 }
 
 const pkgJson = {
-  name: "@davesnx/styled-ppx",
+  name,
   version: esy.version,
   description: esy.description,
   homepage: esy.homepage,
   license: esy.license,
   repository: esy.repository,
   scripts: {
-    postinstall: "node postinstall.js",
+    postinstall: 'node postinstall.js',
   },
   bin: {
-    "styled-ppx": "styled-ppx.exe",
+    'styled-ppx': 'styled-ppx.exe',
   },
   files: [
-    "platform-windows-x64/",
-    "platform-linux-x64/",
-    "platform-darwin-x64/",
-    "postinstall.js",
-    "styled-ppx.exe",
+    'platform-windows-x64/',
+    'platform-linux-x64/',
+    'platform-darwin-x64/',
+    'postinstall.js',
+    'styled-ppx.exe',
   ],
 };
 
 fs.writeFileSync(
-  path.join(dst, "package.json"),
+  path.join(dst, 'package.json'),
   JSON.stringify(pkgJson, null, 2)
 );
