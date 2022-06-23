1079d40c44f7b61a9fc92ec978f058202af8c29d
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-06-23 20:38:31 +0200

    feat: add syntax highlight

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -3,6 +3,12 @@ import "../styles.css";
 import "nextra-theme-docs/style.css";
 import "../overrides.css";
 
+import Prism from "prism-react-renderer/prism";
+
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+
+require("prismjs/components/prism-rescript");
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
