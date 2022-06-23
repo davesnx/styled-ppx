aaeca20e7575ede91f9987e36b373edd2887fb04
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-06-23 20:27:38 +0200

    feat: move overrides outside, change header

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -1,6 +1,7 @@
 import "../fonts.css";
 import "../styles.css";
 import "nextra-theme-docs/style.css";
+import "../overrides.css";
 
 export default function Nextra({ Component, pageProps }) {
   return (
