aee801346694fb744bb509df35bd995b64fdacfe
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-07-28 14:32:06 +0200

    Add syntax highlight

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -10,13 +10,25 @@ const langs = [
     scopeName: 'source.rescript',
     path: Path.join(__dirname, './syntax/rescript.tmLanguage.json'),
   },
-  // This didn't work on nextra v2 beta 11, would be really sick
-  // {
-  //   id: 'styled-ppx',
-  //   scopeName: 'source.styled-ppx-rescript',
-  //   path: Path.join(__dirname, '..', 'editors/vscode/syntaxes/styled-ppx-rescript.json'),
-  //   embeddedLangs: ['css', 'rescript']
-  // },
+// Another day we will try to make all grammars work in the website
+//  {
+//    id: 'styled-ppx-rescript',
+//    scopeName: 'source.styled-ppx-rescript',
+//    path: Path.join(__dirname, '..', 'editors/vscode/syntaxes/styled-ppx-rescript.json'),
+//    embeddedLangs: ['rescript', 'css', 'scss', 'styled-ppx-ocaml', 'styled-ppx-css'],
+//  },
+//  {
+//    id: 'styled-ppx-ocaml',
+//    scopeName: 'source.styled-ppx-ocaml',
+//    path: Path.join(__dirname, '..', 'editors/vscode/syntaxes/styled-ppx-ocaml.json'),
+//    embeddedLangs: ['css', 'scss', 'styled-ppx-css']
+//  },
+//  {
+//    id: 'styled-ppx-css',
+//    scopeName: 'source.css.styled-ppx',
+//    path: Path.join(__dirname, '..', 'editors/vscode/syntaxes/css.styled-ppx.json'),
+//    embeddedLangs: ['css', 'scss']
+//  },
 ];
 
 module.exports = withNextra({
@@ -30,6 +42,7 @@ module.exports = withNextra({
   unstable_flexsearch: true, */
   mdxOptions: {
     rehypePrettyCodeOptions: {
+      theme: 'github-dark-dimmed',
       getHighlighter: options => {
         return getHighlighter({
           ...options,
--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -2,14 +2,6 @@ import "../fonts.css";
 import "../tailwind.css";
 import "nextra-theme-docs/style.css";
 import "../overrides.css";
-import Head from 'next/head'
-
-/* Remove Prism, and prism-react-render after update Nextra to v2,
-in favor of shiki and next.config.js commented code */
-import Prism from "prism-react-renderer/prism";
-(typeof global !== "undefined" ? global : window).Prism = Prism;
-require("prismjs/components/prism-rescript");
-require("prismjs/components/prism-diff");
 
 export default function Nextra({ Component, pageProps }) {
   return (<Component {...pageProps} />);
