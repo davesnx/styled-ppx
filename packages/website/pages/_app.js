106a079a4f5781bcdd776bb7c11700343d80bb39
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-07-28 16:17:55 +0200

    Fix references code, add interpolate fix favicon

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -3,6 +3,45 @@ import "../tailwind.css";
 import "nextra-theme-docs/style.css";
 import "../overrides.css";
 
+import Head from "next/head";
+
 export default function Nextra({ Component, pageProps }) {
-  return (<Component {...pageProps} />);
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`/favicon/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`/favicon/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`/favicon/favicon-16x16.png`}
+        />
+        <link rel="manifest" href={`/favicon/site.webmanifest`} />
+        <link
+          rel="mask-icon"
+          href={`/favicon/safari-pinned-tab.svg`}
+          color="#000000"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- packages/website/theme.config.js
+++ packages/website/theme.config.js
@@ -56,28 +56,28 @@ const Head = (props) => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href={`${url}/favicon/apple-touch-icon.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href={`${url}/favicon/favicon-32x32.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href={`${url}/favicon/favicon-16x16.png`}
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="manifest" href={`${url}/favicon/site.webmanifest`} />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href={`${url}/favicon/safari-pinned-tab.svg`}
         color="#000000"
       />
     </>
-  )
+  );
 };
 
 export default {
