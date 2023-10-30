d5d1f7b206fb6775e2b929fdfe3e23e1fd319f55
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-10-30 17:54:26 +0100

    Fix rescript v9 tests

--- e2e/rescript-v9-JSX3/vite.config.mjs
+++ e2e/rescript-v9-JSX3/vite.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import { externalizeDeps } from 'vite-plugin-externalize-deps'
+import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   test: {
     include: ["*_test.bs.js"],
     globals: true,
     environment: "jsdom",
   },
-  root: "test",
-  plugins: [externalizeDeps()],
+  root: "src",
+  optimizeDeps: {
+    include: ['@emotion/css'],
+  },
+  plugins: [
+    externalizeDeps(),
+    createReScriptPlugin()
+  ],
 })
