09d624522cd33493755676b4439d46e4c088ffbc
Javier Chavarri <javier.chavarri@gmail.com>
2023-11-06 23:45:50 +0000

    hash like emotion

--- packages/emotion-hash/hash.ml
+++ packages/emotion-hash/hash.ml
@@ -1,133 +1,103 @@
-let ( << ) = Int32.shift_left
-let ( & ) = Int32.logand
-let ( ||| ) = Int32.logor
-let ( * ) = Int32.mul
-let ( >>> ) = Int32.shift_right
-let ( ++ ) = Int32.add
-let ( ^ ) = Int32.logxor
-
-let to_base36 (num : Int32.t) =
-  let rec to_base36' (num : Int32.t) =
-    if num = 0l then []
+let to_base36_string n =
+  let open Int32 in
+  let base36_chars = "0123456789abcdefghijklmnopqrstuvwxyz" in
+  let rec convert_to_base36 acc n =
+    if n = 0l then acc
     else (
-      let quotient = Int32.div num 36l in
-      let remainder = Int32.rem num 36l in
-      (match remainder with
-      | 10l -> "a"
-      | 11l -> "b"
-      | 12l -> "c"
-      | 13l -> "d"
-      | 14l -> "e"
-      | 15l -> "f"
-      | 16l -> "g"
-      | 17l -> "h"
-      | 18l -> "i"
-      | 19l -> "j"
-      | 20l -> "k"
-      | 21l -> "l"
-      | 22l -> "m"
-      | 23l -> "n"
-      | 24l -> "o"
-      | 25l -> "p"
-      | 26l -> "q"
-      | 27l -> "r"
-      | 28l -> "s"
-      | 29l -> "t"
-      | 30l -> "u"
-      | 31l -> "v"
-      | 32l -> "w"
-      | 33l -> "x"
-      | 34l -> "y"
-      | 35l -> "z"
-      | _ -> string_of_int (Int32.to_int remainder))
-      :: to_base36' quotient)
+      let quotient = unsigned_div n 36l in
+      let remainder = unsigned_rem n 36l in
+      let char_at_remainder =
+        String.make 1 (String.get base36_chars (to_int remainder))
+      in
+      convert_to_base36 (char_at_remainder :: acc) quotient)
   in
-  num |> to_base36' |> List.rev |> String.concat ""
+  if n = 0l then "0" else String.concat "" (convert_to_base36 [] n)
 
-let to_css (number : Int32.t) = number |> to_base36
+let default str =
+  let open Int32 in
+  let ( lor ) = logor in
+  let ( land ) = logand in
+  let ( lsl ) = shift_left in
+  let ( lsr ) = shift_right_logical in
+  let ( lxor ) = logxor in
+  (* 'm' and 'r' are mixing constants generated offline.
+     They're not really 'magic', they just happen to work well. *)
+  let m = 0x5bd1e995l in
+  let r = 24 in
 
-(*
-    The murmur2 hashing is based on @emotion/hash, which is based on
-    https://github.com/garycourt/murmurhash-js and ported from
-    https://github.com/aappleby/smhasher/blob/61a0530f28277f2e850bfc39600ce61d02b518de/src/MurmurHash2.cpp#L37-L86.
-    Reference: https://github.com/emotion-js/emotion/blob/main/packages/hash/src/index.js
+  (* Initialize the hash *)
+  let h = ref 0l in
 
-    It's an ongoing effort to have a hashing function, currently okish. *)
-let murmur2 (str : string) =
-  let length = String.length str in
-  (* var h = 0 *)
-  let hash = ref 0l in
-  (* var k *)
-  let k = ref 0l in
-  (* len = str.length *)
-  let len = ref length in
-  (* i = 0, *)
-  let index = ref 0 in
-  while !index + 4 <= length do
-    let char_code_1 = Int32.of_int (Char.code str.[!index]) in
-    let char_code_2 = Int32.of_int (Char.code str.[!index + 1]) in
-    let char_code_3 = Int32.of_int (Char.code str.[!index + 2]) in
-    let char_code_4 = Int32.of_int (Char.code str.[!index + 3]) in
-    (* k =
-       (str.charCodeAt(i) & 0xff) |
-       ((str.charCodeAt(++i) & 0xff) << 8) |
-       ((str.charCodeAt(++i) & 0xff) << 16) |
-       ((str.charCodeAt(++i) & 0xff) << 24) *)
-    k :=
-      char_code_1
-      ||| (char_code_2 << 8)
-      ||| (char_code_3 << 16)
-      ||| (char_code_4 << 24);
-    (* k =
-       (k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16) *)
-    k :=
-      ((!k & 0xffffl) * 0x5bd1e995l) ++ (((!k >>> 16) * 0x5bd1e995l) & 0xffffl)
-      << 16;
-    (* k ^=  k >>> 24 *)
-    k := !k ^ (!k >>> 24);
-    k :=
-      ((!k & 0xffffl) * 0x5bd1e995l) ++ (((!k >>> 16) * 0x5bd1e995l) & 0xffffl)
-      << 16;
-    (* h =
-       ((k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16)) ^
-       ((h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16)) *)
-    hash :=
-      (((!hash & 0xffffl) * 0x5bd1e995l)
-       ++ (((!hash >>> 16) * 0x5bd1e995l) & 0xffffl)
-      << 16)
-      ^ !k;
-    index := !index + 1;
+  (* Mix 4 bytes at a time into the hash *)
+  let i = ref 0 in
+  let len = ref (String.length str) in
+  let of_char c = of_int (int_of_char c) in
+  while !len >= 4 do
+    let k =
+      of_char str.[!i]
+      land 0xffl
+      lor ((of_char str.[!i + 1] land 0xffl) lsl 8)
+      lor ((of_char str.[!i + 2] land 0xffl) lsl 16)
+      lor ((of_char str.[!i + 3] land 0xffl) lsl 24)
+    in
+
+    let k = add (mul (k land 0xffffl) m) (mul (k lsr 16) 0xe995l lsl 16) in
+    print_endline (Printf.sprintf "K INIT: %lx" k);
+    let k = k lxor (k lsr r) in
+
+    print_endline (Printf.sprintf "K FINAL: %lx" k);
+    h :=
+      add (mul (k land 0xffffl) m) (mul (k lsr 16) 0xe995l lsl 16)
+      lxor add (mul (!h land 0xffffl) m) (mul (!h lsr 16) 0xe995l lsl 16);
+
+    i := !i + 4;
     len := !len - 4
+    (* print_endline (Printf.sprintf "loop %d" !i);
+       print_endline (Printf.sprintf "len %d" !len);
+       print_endline (Printf.sprintf "H is: %lx" !h) *)
   done;
 
+  (* print_endline (Printf.sprintf "len: %d" !len);
+     print_endline (Printf.sprintf "i: %d" !i);
+     print_endline (Printf.sprintf "H is: %lx" !h); *)
+
   (* Handle the last few bytes of the input array *)
-  (match !len with
-  | 3 ->
-    (* h ^= (str.charCodeAt(i + 2) & 0xff) << 16 *)
-    hash := !hash ^ (Int32.of_int (Char.code str.[!index + 2]) << 16)
-  | 2 ->
-    (* h ^= (str.charCodeAt(i + 1) & 0xff) << 8 *)
-    hash := !hash ^ (Int32.of_int (Char.code str.[!index + 1]) << 8)
-  | 1 ->
-    (* h ^= str.charCodeAt(i) & 0xff *)
-    hash := !hash ^ Int32.of_int (Char.code str.[!index]);
-    (* h =
-       (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16) *)
-    hash :=
-      ((!hash & 0xffffl) * 0x5bd1e995l)
-      ++ (((!hash >>> 16) * 0x5bd1e995l) & 0xffffl)
-      << 16
-  | _ -> ());
+  let () =
+    if !len = 3 then h := !h lxor ((of_char str.[!i + 2] land 0xffl) lsl 16);
+    if !len >= 2 then begin
+      (* print_endline "here";
+         print_endline str;
+         print_endline (Printf.sprintf "INDXE %d" (!i + 1));
+         print_endline
+           (Printf.sprintf "ofchar %ld" (!h lxor ((0x20l land 0xffl) lsl 8))); *)
+      h := !h lxor ((of_char str.[!i + 1] land 0xffl) lsl 8)
+    end;
+    if !len >= 1 then begin
+      h := !h lxor (of_char str.[!i] land 0xffl);
+      h := add (mul (!h land 0xffffl) m) (mul (!h lsr 16) 0xe995l lsl 16)
+    end
+  in
 
-  (* Do a few final mixes of the hash to ensure the last few bytes are well-incorporated. *)
+  (* Do a few final mixes of the hash to ensure the last few
+     bytes are well-incorporated. *)
+  h := !h lxor (!h lsr 13);
+  let () = print_endline (Printf.sprintf "ONE %ld" (mul (!h land 0xffffl) m)) in
+  let () =
+    print_endline (Printf.sprintf "TWO %ld" (mul (!h lsr 16) 0xe995l lsl 16))
+  in
+  h :=
+      add
+        (mul (!h land 0xffffl) (m))
+        (mul (!h lsr 16) 0xe995l lsl 16);
+  let () = print_endline (Printf.sprintf "H prealmostfinal is: %lx" !h) in
+  let () = print_endline (Printf.sprintf "H shifted is: %lx" (!h lsr 15)) in
+  let () =
+    print_endline
+      (Printf.sprintf "H test is: %s" (to_base36_string 0xf77f8879l))
+  in
 
-  (* h ^= h >>> 13 *)
-  hash := !hash ^ (!hash >>> 13);
-  (* h =
-     (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16) *)
-  hash :=
-    ((!hash & 0xffffl) * 0x5bd1e995l) ++ ((!hash >>> 16) * 0x5bd1e995l << 16);
-  (* (h ^ (h >>> 15)) >>> 0 *)
-  !hash ^ (!hash >>> 15) >>> 0
-
-let default (str : string) = str |> murmur2 |> to_css
+  print_endline (Printf.sprintf "H FINAL is: %lx" !h);
+  print_endline
+    (Printf.sprintf "FINAL %s"
+       (to_base36_string (!h lxor (!h lsr 15) land 0xffffffffl)));
+  to_base36_string (!h lxor (!h lsr 15) land 0xffffffffl)
