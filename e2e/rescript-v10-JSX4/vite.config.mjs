b007748b8eb7fe44f7709df1f7c8380c47462e5b
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-11-14 10:53:42 +0100

    Add emotion/css since I can't make v9 work without it

--- e2e/rescript-v10-JSX4/vite.config.mjs
+++ e2e/rescript-v10-JSX4/vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import { externalizeDeps } from 'vite-plugin-externalize-deps'
-import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
+/* import { externalizeDeps } from 'vite-plugin-externalize-deps' */
+/* import createReScriptPlugin from '@jihchi/vite-plugin-rescript'; */
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
