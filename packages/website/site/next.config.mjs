356e7864de86a0c881d1eb9777ada9a6ad7f35de
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-03-25 16:56:51 +0100

    Inline most grammar creations

--- packages/website/site/next.config.mjs
+++ packages/website/site/next.config.mjs
@@ -1,125 +1,92 @@
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import fs from "node:fs";
+import Path from "node:path";
+import URL from "node:url";
+import Fs from "node:fs";
 import nextra from "nextra";
-import { getHighlighter } from "shiki";
+import * as Shiki from "shiki";
 import { bundledLanguages } from "shiki/langs";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const rescriptGrammar = JSON.parse(
-  fs.readFileSync(join(__dirname, "syntaxes/rescript.tmLanguage.json"), "utf8")
-);
-const reasonGrammar = JSON.parse(
-  fs.readFileSync(join(__dirname, "syntaxes/reason.tmLanguage.json"), "utf8")
-);
+const __dirname = Path.dirname(URL.fileURLToPath(import.meta.url));
+
+const syntaxes = Path.join(__dirname, "syntaxes");
+const editors = Path.join(__dirname, "..", "..", "editors");
+
+const rescriptGrammar = {
+  ...JSON.parse(
+    Fs.readFileSync(Path.join(syntaxes, "rescript.tmLanguage.json"), "utf8")
+  ),
+  name: "rescript",
+};
+
+const reasonGrammar = {
+  ...JSON.parse(
+    Fs.readFileSync(Path.join(syntaxes, "reason.tmLanguage.json"), "utf8")
+  ),
+  name: "reason",
+};
+
 const styledPpxCssGrammar = JSON.parse(
-  fs.readFileSync(
-    join(__dirname, "../..", "editors/vscode/syntaxes/css.styled-ppx.json"),
-    "utf8"
-  )
-);
-const styledPpxOCamlGrammar = JSON.parse(
-  fs.readFileSync(
-    join(__dirname, "../..", "editors/vscode/syntaxes/styled-ppx-ocaml.json"),
-    "utf8"
-  )
-);
-const styledPpxReScriptGrammar = JSON.parse(
-  fs.readFileSync(
-    join(
-      __dirname,
-      "../..",
-      "editors/vscode/syntaxes/styled-ppx-rescript.json"
-    ),
-    "utf8"
-  )
-);
-const styledPpxReasonGrammar = JSON.parse(
-  fs.readFileSync(
-    join(__dirname, "../..", "editors/vscode/syntaxes/styled-ppx-reason.json"),
+  Fs.readFileSync(
+    Path.join(editors, "vscode/syntaxes/css.styled-ppx.json"),
     "utf8"
   )
 );
 
-const langs = [
-  ...Object.keys(bundledLanguages),
-  {
-    //id: "rescript",
-    //scopeName: "source.rescript",
-    //path: join(__dirname, "syntaxes/rescript.tmLanguage.json"),
-    ...rescriptGrammar,
-    name: "rescript",
-  },
-  {
-    //id: "reason",
-    //scopeName: "source.reason",
-    //path: join(__dirname, "syntaxes/reason.tmLanguage.json"),
-    ...reasonGrammar,
-    name: "reason",
-  },
-  {
-    //id: "styled-ppx-css",
-    //scopeName: "source.css.styled-ppx",
-    //path: join(
-    //  __dirname,
-    //  "../..",
-    //  "editors/vscode/syntaxes/css.styled-ppx.json"
-    //),
-    ...styledPpxCssGrammar,
-    name: "styled-ppx-css",
-  },
-  {
-    //id: "styled-ppx-ocaml",
-    //scopeName: "source.styled-ppx-ocaml",
-    //path: join(
-    //  __dirname,
-    //  "../..",
-    //  "editors/vscode/syntaxes/styled-ppx-ocaml.json"
-    //),
-    ...styledPpxOCamlGrammar,
-    injectTo: ["source.ocaml"],
-    name: "styled-ppx-ocaml",
-  },
-  {
-    //id: "styled-ppx-rescript",
-    //scopeName: "source.styled-ppx-rescript",
-    //path: join(
-    //  __dirname,
-    //  "../..",
-    //  "editors/vscode/syntaxes/styled-ppx-rescript.json"
-    //),
-    ...styledPpxReScriptGrammar,
-    injectTo: ["source.rescript"],
-    name: "styled-ppx-rescript",
-  },
-  {
-    //id: "styled-ppx-reason",
-    //scopeName: "source.styled-ppx-reason",
-    //injectTo: ["source.reason"],
-    //path: join(
-    //  __dirname,
-    //  "../..",
-    //  "editors/vscode/syntaxes/styled-ppx-reason.json"
-    //),
-    ...styledPpxReasonGrammar,
-    injectTo: ["source.reason"],
-    name: "styled-ppx-reason",
-  },
-];
+const styledPpxOCamlGrammar = {
+  ...JSON.parse(
+    Fs.readFileSync(
+      Path.join(editors, "vscode/syntaxes/styled-ppx-ocaml.json"),
+      "utf8"
+    )
+  ),
+  injectTo: ["source.ocaml"],
+};
+
+const styledPpxReScriptGrammar = {
+  ...JSON.parse(
+    Fs.readFileSync(
+      Path.join(editors, "vscode/syntaxes/styled-ppx-rescript.json"),
+      "utf8"
+    )
+  ),
+  injectTo: ["source.rescript"],
+};
+
+const styledPpxReasonGrammar = {
+  ...JSON.parse(
+    Fs.readFileSync(
+      Path.join(editors, "vscode/syntaxes/styled-ppx-reason.json"),
+      "utf8"
+    )
+  ),
+  injectTo: ["source.reason"],
+};
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.js",
+  themeConfig: "./theme.config.tsx",
   mdxOptions: {
     rehypePrettyCodeOptions: {
       theme: { light: "github-light", dark: "github-dark-dimmed" },
       getHighlighter: (options) =>
-        getHighlighter({
+        Shiki.getHighlighter({
           ...options,
-          langs,
+          langs: [
+            ...Object.keys(bundledLanguages),
+            rescriptGrammar,
+            reasonGrammar,
+            styledPpxCssGrammar,
+            styledPpxOCamlGrammar,
+            styledPpxReScriptGrammar,
+            styledPpxReasonGrammar,
+          ],
         }),
     },
   },
 });
 
-export default withNextra({});
+/**
+ * @type {import('next').Config}
+ */
+const nextConfig = {};
+
+export default withNextra(nextConfig);
