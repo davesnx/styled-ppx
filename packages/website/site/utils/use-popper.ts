1668e85a3190c23abaab496bef60724d76f80902
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-04-11 19:46:39 +0200

    Modify diffs

--- packages/website/site/utils/use-popper.ts
+++ packages/website/site/utils/use-popper.ts
@@ -1,7 +1,7 @@
-import type { Options } from '@popperjs/core'
-import { createPopper } from '@popperjs/core'
-import type { RefCallback } from 'react'
-import { useCallback, useMemo, useRef } from 'react'
+import type { Options } from "@popperjs/core";
+import { createPopper } from "@popperjs/core";
+import type { RefCallback } from "react";
+import { useCallback, useMemo, useRef } from "react";
 
 /**
  * https://github.com/tailwindlabs/headlessui/issues/59
@@ -10,33 +10,33 @@ import { useCallback, useMemo, useRef } from 'react'
 export function usePopper(
   options?: Partial<Options>
 ): [RefCallback<Element | null>, RefCallback<HTMLElement | null>] {
-  const reference = useRef<Element | null>(null)
-  const popper = useRef<HTMLElement | null>(null)
+  const reference = useRef<Element | null>(null);
+  const popper = useRef<HTMLElement | null>(null);
 
-  const cleanupCallback = useRef<() => void>()
+  const cleanupCallback = useRef<() => void>();
 
   const instantiatePopper = useCallback(() => {
-    if (!reference.current || !popper.current) return
+    if (!reference.current || !popper.current) return;
 
-    cleanupCallback.current?.()
+    cleanupCallback.current?.();
     cleanupCallback.current = createPopper(
       reference.current,
       popper.current,
       options
-    ).destroy
-  }, [reference, popper, cleanupCallback, options])
+    ).destroy;
+  }, [reference, popper, cleanupCallback, options]);
 
   return useMemo(
     () => [
-      referenceDomNode => {
-        reference.current = referenceDomNode
-        instantiatePopper()
+      (referenceDomNode) => {
+        reference.current = referenceDomNode;
+        instantiatePopper();
       },
-      popperDomNode => {
-        popper.current = popperDomNode
-        instantiatePopper()
-      }
+      (popperDomNode) => {
+        popper.current = popperDomNode;
+        instantiatePopper();
+      },
     ],
     [reference, popper, instantiatePopper]
-  )
+  );
 }
