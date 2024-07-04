b2e35a50006ecdd14c574ecc3ce6c940ab8ff40e
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-07-04 16:42:44 +0200

    Remove Css_Legacy_Core

--- packages/emotion/test/test.ml
+++ packages/emotion/test/test.ml
@@ -1,8 +1,5 @@
 let () =
   Alcotest.run ~show_errors:true ~compact:true ~tail_errors:`Unlimited "Emotion"
     [
-      Test_css_js_styles.tests;
-      Test_css_styles.tests;
-      Test_css_autoprefixer.tests;
-      Test_css_hash.tests;
+      Test_css_js_styles.tests; Test_css_autoprefixer.tests; Test_css_hash.tests;
     ]
