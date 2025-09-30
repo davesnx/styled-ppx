e93e1e5c0f8260313d9ca9bd35aca05d12e6e3e7
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-09-30 10:41:14 +0200

    Remove leftover cram tests

--- packages/ppx/test/css-support/test_cx2.ml
+++
@@ -1,8 +0,0 @@
-let css main =
-  [%cx2
-    {|
-  color: $(main);
-  background-color: $(CSS.black);
-  display: flex;
-|}]
-;;((div ~className:(css CSS.red) ~children:[] ())[@JSX ])
--- packages/ppx/test/css-support/test_cx2_output2.ml
+++
@@ -1,8 +0,0 @@
-let css main =
-  CSS.make "css-3tpy8b css-1uzc9um css-17vxl0k"
-    [("--var-19ja411", (CSS.get_value_from_rule (CSS.color main)));
-    ("--var-1xt8d8f",
-      (CSS.get_value_from_rule (CSS.backgroundColor CSS.black)))]
-;;((div ~className:(css CSS.red) ~children:[] ())[@JSX ])
-[@@@EXTRACT_CSS
-  ".css-3tpy8b { color: var(--var-19ja411); }\n.css-1uzc9um { background-color: var(--var-1xt8d8f); }\n.css-17vxl0k { display: flex; }\n"]
