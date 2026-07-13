f0d39c0dc5806af781dd96a7bcc2d03e2ed03636
David Sancho Moreno <david.sancho@ahrefs.com>
2026-07-13 09:38:55 +0000

    Alias: mirror trivial constructors literally instead of eta-expanding
    An eta-expanded body still references the Css_types submodule object, so
    first use of e.g. CSS.rgb rooted Color's whole printer machinery.
    Trivial constructors now replicate the variant construction with the
    same parameter types; only Shadow.*/Animation/Transition shorthands
    (real logic) stay eta-expanded.
    
    Result: {"status":"keep","floor_total_bytes":603,"sink_total_bytes":15581,"demo_runtime_bytes":10105}

--- packages/runtime/native/shared/Alias.ml
+++ packages/runtime/native/shared/Alias.ml
@@ -12,27 +12,27 @@ let var ?default (x : string) : [> Var.t ] =
 let auto = `auto
 let none = `none
 let text = `text
-let pct x = Percentage.pct x
-let num x = Length.num x
-let ch x = Length.ch x
-let cm x = Length.cm x
-let em x = Length.em x
-let ex x = Length.ex x
-let mm x = Length.mm x
-let pt x = Length.pt x
-let px x = Length.px x
-let pxFloat x = Length.pxFloat x
-let rem x = Length.rem x
-let vh x = Length.vh x
-let vw x = Length.vw x
-let vmin x = Length.vmin x
-let vmax x = Length.vmax x
+let pct (x : float) = `percent x
+let num (x : float) = `num x
+let ch (x : float) = `ch x
+let cm (x : float) = `cm x
+let em (x : float) = `em x
+let ex (x : float) = `ex x
+let mm (x : float) = `mm x
+let pt (x : int) = `pt x
+let px (x : int) = `px x
+let pxFloat (x : float) = `pxFloat x
+let rem (x : float) = `rem x
+let vh (x : float) = `vh x
+let vw (x : float) = `vw x
+let vmin (x : float) = `vmin x
+let vmax (x : float) = `vmax x
 let zero = `zero
-let fr x = TrackBreadth.fr x
-let deg x = Angle.deg x
-let rad x = Angle.rad x
-let grad x = Angle.grad x
-let turn x = Angle.turn x
+let fr (x : float) = `fr x
+let deg (x : float) = `deg x
+let rad (x : float) = `rad x
+let grad (x : float) = `grad x
+let turn (x : float) = `turn x
 let ltr = `ltr
 let rtl = `rtl
 let absolute = `absolute
@@ -49,11 +49,16 @@ let oblique = `oblique
 let hidden = `hidden
 let visible = `visible
 let scroll = `scroll
-let rgb r g b = Color.rgb r g b
-let rgba r g b a = Color.rgba r g b a
-let hsl h s l = Color.hsl h s l
-let hsla h s l a = Color.hsla h s l a
-let hex x = Color.hex x
+let rgb (r : int) (g : int) (b : int) = `rgb (r, g, b)
+let rgba (r : int) (g : int) (b : int) (a : Color.alpha_with_calc) =
+  `rgba (r, g, b, a)
+let hsl (h : Color.angle_with_calc) (s : Color.percent_with_calc)
+  (l : Color.percent_with_calc) =
+  `hsl (h, s, l)
+let hsla (h : Color.angle_with_calc) (s : Color.percent_with_calc)
+  (l : Color.percent_with_calc) (a : Color.angle_with_calc) =
+  `hsla (h, s, l, a)
+let hex (x : string) = `hex x
 let currentColor = `currentColor
 let transparent = `transparent
 let linear = `linear
@@ -63,28 +68,30 @@ let easeInOut = `easeInOut
 let easeOut = `easeOut
 let stepStart = `stepStart
 let stepEnd = `stepEnd
-let steps i dir = EasingFunction.steps i dir
-let cubicBezier a b c d = EasingFunction.cubicBezier a b c d
+let steps (i : int) (dir : [ `start | `end_ ]) = `steps (i, dir)
+let cubicBezier (a : float) (b : float) (c : float) (d : float) =
+  `cubicBezier (a, b, c, d)
 let marginBox = `marginBox
 let fillBox = `fillBox
 let strokeBox = `strokeBox
 let viewBox = `viewBox
-let translate x y = Transform.translate x y
-let translate3d x y z = Transform.translate3d x y z
-let translateX x = Transform.translateX x
-let translateY y = Transform.translateY y
-let translateZ z = Transform.translateZ z
-let scaleX x = Transform.scaleX x
-let scaleY x = Transform.scaleY x
-let scaleZ x = Transform.scaleZ x
-let rotateX a = Transform.rotateX a
-let rotateY a = Transform.rotateY a
-let rotateZ a = Transform.rotateZ a
-let scale x y = Transform.scale x y
-let scale3d x y z = Transform.scale3d x y z
-let skew a a' = Transform.skew a a'
-let skewX a = Transform.skewX a
-let skewY a = Transform.skewY a
+let translate (x : Length.t) (y : Length.t) = `translate (x, y)
+let translate3d (x : Length.t) (y : Length.t) (z : Length.t) =
+  `translate3d (x, y, z)
+let translateX (x : Length.t) = `translateX x
+let translateY (y : Length.t) = `translateY y
+let translateZ (z : Length.t) = `translateZ z
+let scaleX (x : float) = `scaleX x
+let scaleY (x : float) = `scaleY x
+let scaleZ (x : float) = `scaleZ x
+let rotateX (a : Angle.t) = `rotateX a
+let rotateY (a : Angle.t) = `rotateY a
+let rotateZ (a : Angle.t) = `rotateZ a
+let scale (x : float) (y : float) = `scale (x, y)
+let scale3d (x : float) (y : float) (z : float) = `scale3d (x, y, z)
+let skew (a : Angle.t) (a' : float) = `skew (a, a')
+let skewX (a : Angle.t) = `skewX a
+let skewY (a : Angle.t) = `skewY a
 let thin = `thin
 let extraLight = `extraLight
 let light = `light
@@ -94,26 +101,35 @@ let bold = `bold
 let extraBold = `extraBold
 let lighter = `lighter
 let bolder = `bolder
-let linearGradient direction stops = Gradient.linearGradient direction stops
+let linearGradient (direction : Gradient.direction option)
+  (stops : Gradient.color_stop_list) =
+  `linearGradient (Some direction, stops)
 
-let repeatingLinearGradient direction stops =
-  Gradient.repeatingLinearGradient direction stops
+let repeatingLinearGradient (direction : Gradient.direction option)
+  (stops : Gradient.color_stop_list) =
+  `repeatingLinearGradient (Some direction, stops)
 
-let radialGradient shape size position stops =
-  Gradient.radialGradient shape size position stops
+let radialGradient (shape : Gradient.shape) (size : Gradient.radial_size)
+  (position : Position.t) (stops : Gradient.color_stop_list) =
+  `radialGradient (Some shape, Some size, Some position, stops)
 
-let repeatingRadialGradient shape size position stops =
-  Gradient.repeatingRadialGradient shape size position stops
+let repeatingRadialGradient (shape : Gradient.shape)
+  (size : Gradient.radial_size) (position : Position.t)
+  (stops : Gradient.color_stop_list) =
+  `repeatingRadialGradient (Some shape, Some size, Some position, stops)
 
-let conicGradient angle stops = Gradient.conicGradient angle stops
-let area x = ExplicitTrackWithArea.area x
-let areas x = GridTemplateAreas.areas x
-let trackSizes x = GridAutoColumns.trackSizes x
-let tracks x = GridTemplateRows.Value.tracks x
-let numInt x = GridLine.num x
-let ident x = GridLine.ident x
-let numIdent x y = GridLine.numIdent x y
-let span x = GridLine.span x
+let conicGradient (angle : Gradient.direction option)
+  (stops : Gradient.color_stop_list) =
+  `conicGradient (Some angle, stops)
+let area (x : string) = `area x
+let areas (x : string array) = `areas x
+let trackSizes (x : TrackSize.t array) = `trackSizes x
+let tracks (x : Track.t array) = `tracks x
+let numInt (x : int) = `num x
+let ident (x : string) = `ident x
+let numIdent (x : int) (y : string) = `numIdent (x, y)
+let span (x : [ `num of int | `ident of string | `numIdent of int * string ]) =
+  `span x
 let contextMenu = `contextMenu
 let help = `help
 let pointer = `pointer
@@ -153,7 +169,7 @@ module Calc = struct
   let ( * ) a b = `calc (`mult (a, b))
 end
 
-let size x y = BackgroundSize.Value.size x y
+let size (x : [ Length.t | Auto.t ]) (y : [ Length.t | Auto.t ]) = `size (x, y)
 let all = `all
 let backwards = `backwards
 let baseline = `baseline
@@ -199,13 +215,14 @@ let space = `space
 let nowrap = `nowrap
 let paddingBox = `paddingBox
 let paused = `paused
-let repeatFn x y = Repeat.repeat x y
+let repeatFn (x : RepeatValue.t) (y : RepeatTrack.t array) = `repeat (x, y)
 let repeat = `repeat
-let minmax x y = MinMax.minmax x y
+let minmax (x : InflexibleBreadth.t) (y : TrackBreadth.t) = `minmax (x, y)
 let repeatX = `repeatX
 let repeatY = `repeatY
-let rotate a = Transform.rotate a
-let rotate3d x y z a = Transform.rotate3d x y z a
+let rotate (a : Angle.t) = `rotate a
+let rotate3d (x : float) (y : float) (z : float) (a : Angle.t) =
+  `rotate3d (x, y, z, a)
 let row = `row
 let rowReverse = `rowReverse
 let running = `running
@@ -214,7 +231,7 @@ let spaceAround = `spaceAround
 let spaceBetween = `spaceBetween
 let spaceEvenly = `spaceEvenly
 let stretch = `stretch
-let url x = Url.url x
+let url (x : string) = `url x
 let wrap = `wrap
 let wrapReverse = `wrapReverse
 let inside = `inside
@@ -245,7 +262,7 @@ let fill = `fill
 let maxContent = `maxContent
 let minContent = `minContent
 let fitContent = `fitContent
-let fitContentFn x = TrackSize.fitContent x
+let fitContentFn (x : Length.t) = `fitContent x
 let round = `round
 let miter = `miter
 let bevel = `bevel
@@ -260,7 +277,7 @@ let panDown = `panDown
 let pinchZoom = `pinchZoom
 let manipulation = `manipulation
 let subgrid = `subgrid
-let lineNames x = Track.lineNames x
+let lineNames (x : string) = `lineNames x
 
 module Shadow = struct
   type box = Css_types.Shadow.box Css_types.Shadow.t
